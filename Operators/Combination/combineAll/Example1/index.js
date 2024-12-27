4f2e6538f7506806ffdb37fb041840c40c01fd36
Tharindu37 <tharindut520@gmail.com>
2024-12-28 02:29:29 +0530

    update

--- Operators/Combination/combineAll/Example1/index.js
+++ Operators/Combination/combineAll/Example1/index.js
@@ -5,7 +5,15 @@ var rxjs_1 = require("rxjs");
 // emit every 1s, take 2
 var source$ = (0, rxjs_1.interval)(1000).pipe((0, operators_1.take)(2));
 // map each emitted value from source to interval observable that takes 5 values
-var example$ = source$.pipe((0, operators_1.map)(function (val) { return (0, rxjs_1.interval)(1000).pipe((0, operators_1.map)(function (i) { return "Result (".concat(val, "): ").concat(i); }), (0, operators_1.take)(5)); }));
+// const example$ = source$.pipe(
+//     map(val => interval(1000).pipe(map(i=> `Result (${val}): ${i}`), take(5)))
+// );
+var example$ = source$.pipe((0, operators_1.map)(function (val) { return (0, rxjs_1.interval)(1000).pipe((0, operators_1.map)(function (i) { console.log(val, i); return "Result (".concat(val, "): ").concat(i); }), (0, operators_1.take)(5)); }));
+/*
+  2 values from source will map to 2 (inner) interval observables that emit every 1s.
+  combineAll uses combineLatest strategy, emitting the last value from each
+  whenever either observable emits a value
+*/
 // example$.subscribe(val => console.log(val));
 example$.pipe((0, operators_1.combineAll)())
     /*
--- Operators/Combination/combineAll/Example1/index.ts
+++ Operators/Combination/combineAll/Example1/index.ts
@@ -4,10 +4,17 @@ import { interval } from "rxjs";
 // emit every 1s, take 2
 const source$ = interval(1000).pipe(take(2));
 // map each emitted value from source to interval observable that takes 5 values
+// const example$ = source$.pipe(
+//     map(val => interval(1000).pipe(map(i=> `Result (${val}): ${i}`), take(5)))
+// );
 const example$ = source$.pipe(
-    map(val => interval(1000).pipe(map(i=> `Result (${val}): ${i}`), take(5)))
+    map(val => interval(1000).pipe(map(i=> {console.log(val,i); return `Result (${val}): ${i}`}), take(5)))
 );
-
+/*
+  2 values from source will map to 2 (inner) interval observables that emit every 1s.
+  combineAll uses combineLatest strategy, emitting the last value from each
+  whenever either observable emits a value
+*/
 // example$.subscribe(val => console.log(val));
 example$.pipe(combineAll())
 /*
